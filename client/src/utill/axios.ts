fed9ccf529a1d4b3eb868a96fa7f19237c93b515
blukoo <dnehdrkd123@naver.com>
2022-08-20 23:54:29 +0900

    [refactor]서버와 추가을 위해 프론트는 client 폴더에

---
+++ client/src/utill/axios.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+import { unknownObj, AxiosResponse } from 'types/index';
+export const API_KEY = '4872db6098f1cdb0d58bb445f402dd28'; // TMDB API Key
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+instance.interceptors.response.use(
+  function (config) {
+    return config.data as AxiosResponse;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+const api = {
+  get: async ({
+    url,
+    query,
+    many = false,
+  }: {
+    url: string;
+    query: any;
+    many: Boolean;
+  }) => {
+    const method = 'get';
+    const params = query;
+    const result = await instance({
+      url,
+      method,
+      params,
+    });
+    return (result as AxiosResponse) || (many ? [] : {});
+  },
+  post: ({ url, query }: { url: any; query: any }) => {
+    const params = query;
+    return instance.post(url, params);
+  },
+  delete: ({ url, query }: { url: any; query: any }) => {
+    const params = query;
+    return instance.delete(url, {
+      data: params,
+    });
+  },
+  put: ({ url, query }: { url: any; query: any }) => {
+    const params = query;
+    return instance.put(url, params);
+  },
+};
+export default api;
