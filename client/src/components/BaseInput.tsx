e7e4e515262eaba05858c0c654b445564a28c9fc
blukoo <dnehdrkd123@naver.com>
2022-09-07 23:42:22 +0900

    [fix]baseInput beforelabel props 추가

--- client/src/components/BaseInput.tsx
+++ client/src/components/BaseInput.tsx
@@ -1,9 +1,19 @@
 import { forwardRef, useRef } from 'react';
 const BaseInput = forwardRef(
-  (props: { [key: string]: any }, ref: React.Ref<HTMLInputElement>) => {
+  (props: { [key: string]: any }, ref?: React.Ref<HTMLInputElement>) => {
     return (
       <>
-        <input {...props} ref={ref} />
+        {props.beforelabel ? (
+          <label htmlFor={props.id} style={props.style.label}>
+            {props.beforelabel}
+          </label>
+        ) : null}
+
+        <input
+          {...props}
+          style={props.style ? props.style.input : null}
+          ref={ref}
+        />
         {props.label ? <label htmlFor={props.id}></label> : null}
       </>
     );
