3b697b5ea740b7bee999f1aa0d33cc943ce5e6b1
blukoo <dnehdrkd123@naver.com>
2022-09-08 21:00:29 +0900

    [fix]인풋 스타일

--- client/src/components/BaseInput.tsx
+++ client/src/components/BaseInput.tsx
@@ -1,10 +1,16 @@
+import style from 'styled/BaseInput.module.css';
 import { forwardRef, useRef } from 'react';
+import styled from 'styled-components';
 const BaseInput = forwardRef(
   (props: { [key: string]: any }, ref?: React.Ref<HTMLInputElement>) => {
     return (
-      <>
+      <div className={style.input_wrap}>
         {props.beforelabel ? (
-          <label htmlFor={props.id} style={props.style.label}>
+          <label
+            htmlFor={props.id}
+            style={props.style.label}
+            className={style.label}
+          >
             {props.beforelabel}
           </label>
         ) : null}
@@ -13,9 +19,12 @@ const BaseInput = forwardRef(
           {...props}
           style={props.style ? props.style.input : null}
           ref={ref}
+          className={style.input}
         />
-        {props.label ? <label htmlFor={props.id}></label> : null}
-      </>
+        {props.label ? (
+          <label htmlFor={props.id} className={style.label}></label>
+        ) : null}
+      </div>
     );
   }
 );
