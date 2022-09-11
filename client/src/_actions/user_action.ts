dfa272b7c17a9977349f3a0c68358a09847ec9d5
blukoo <dnehdrkd123@naver.com>
2022-09-11 20:55:06 +0900

    [feat]axios util custom hook적용중

--- client/src/_actions/user_action.ts
+++ client/src/_actions/user_action.ts
@@ -1,15 +1,25 @@
-import axios from 'axios';
+// import axios from 'axios';
+import UseFetch from 'utill/UseFetch';
 import { LOGIN_USER, REGISTER_USER } from './types';
 export function loginUser(dataToSubmit: any) {
-  const request = axios
-    .post('api/users/login', dataToSubmit)
-    .then(response => response.data);
+  const request = UseFetch({
+    method: 'post',
+    url: 'api/users/login',
+    data: dataToSubmit,
+  });
+  // axios
+  //   .post('api/users/login', dataToSubmit)
+  //   .then(response => response.data);
   return { type: LOGIN_USER, payload: request };
 }
-
 export function registerUser(dataToSubmit: any) {
-  const request = axios
-    .post('api/users/register', dataToSubmit)
-    .then(response => response.data);
+  const request = UseFetch({
+    url: 'api/users/register',
+    data: dataToSubmit,
+    method: 'post',
+  });
+  // axios
+  //   .post('api/users/register', dataToSubmit)
+  //   .then(response => response.data);
   return { type: REGISTER_USER, payload: request };
 }
---
+++ client/src/utill/UseFetch.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { loadingState } from '../atoms';
+import axios from 'axios';
+type Methods = 'head' | 'options' | 'put' | 'post' | 'patch' | 'delete' | 'get';
+function UseFetch({
+  url,
+  method = 'get',
+  data,
+}: {
+  url: string;
+  method: Methods;
+  data: any;
+}) {
+  //   const [response, setResponse] = useState([]);
+  //   const [loading, setLoading] = useRecoilState(loadingState);
+
+  return axios[method](url, data).then((response: any) => response.data);
+}
+export default UseFetch;
